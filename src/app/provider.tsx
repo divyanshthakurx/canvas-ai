b3c7e2164a6aad79e4e8bb0b205d582ae178bb37
divyanshthakurx <divyanshthakurx@gmail.com>
2026-09-03 19:46:48 +0530

    feat: add user detail context and integrate axios for user creation

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,13 +1,34 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { userDetailContext } from '@/context/userDetailContext';
 
 function Provider({children}: {children: React.ReactNode}) {
 
+  const [userDetail, setUserDetail] = useState<any>();
+
+  const createNewUser = async () => {
+    const response = await axios.post("/api/users");
+    console.log(response.data);  
+    setUserDetail(response.data)
+  }
   
+  useEffect(() => {
+    try {
+      createNewUser();
+    } catch (error) {
+      console.log('error is', error);
+    }
+  }, [])
 
   return (
-    <div>{children}</div>
+    // here we pass default value so after passing the value later it is available to all childrens
+    <userDetailContext.Provider value={{userDetail, setUserDetail}}> 
+    <div>
+      {children}
+    </div>
+    </userDetailContext.Provider>
   )
 }
 
-export default Provider
+export default Provider;
